905f0fc6e0c92a34ce8ea3a71b399bd6ecd48065
Chris Weathers <chris@doubleencore.com>
2015-01-23 23:04:28 -0700

    Added HackScene to project.

---
+++ ios/GlobalGameJam/GlobalGameJam/HackScene.m
@@ -0,0 +1,34 @@
+//
+//  HackScene.m
+//  GlobalGameJam
+//
+//  Created by Chris Weathers on 1/23/15.
+//  Copyright (c) 2015 shoshinboogie. All rights reserved.
+//
+
+#import "HackScene.h"
+
+@implementation HackScene
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSLog(@"TEST");
+//    /* Called when a touch begins */
+//    
+//    for (UITouch *touch in touches) {
+//        CGPoint location = [touch locationInNode:self];
+//        
+//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+//        
+//        sprite.xScale = 0.5;
+//        sprite.yScale = 0.5;
+//        sprite.position = location;
+//        
+//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+//        
+//        [sprite runAction:[SKAction repeatActionForever:action]];
+//        
+//        [self addChild:sprite];
+//    }
+}
+
+@end
