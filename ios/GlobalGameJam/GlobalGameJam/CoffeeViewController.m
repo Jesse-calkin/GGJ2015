cae06db3ef61dd982164a2861e2679b9274ae222
Chris Weathers <chris@doubleencore.com>
2015-01-24 12:28:29 -0700

    Hooked up UIViewController+Additions in CoffeeViewController.

--- ios/GlobalGameJam/GlobalGameJam/CoffeeViewController.m
+++ ios/GlobalGameJam/GlobalGameJam/CoffeeViewController.m
@@ -7,26 +7,15 @@
 //
 
 #import "CoffeeViewController.h"
-#import "CoffeeScene.h"
 #import "SceneManager.h"
-
-@interface CoffeeViewController ()
-@property (strong, nonatomic) CoffeeScene *coffeeScene;
-@property (strong, nonatomic) SKView *skView;
-@end
+#import "UIViewController+Additions.h"
 
 @implementation CoffeeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    CGRect skViewFrame = self.view.bounds;
-    self.skView = [[SKView alloc] initWithFrame:skViewFrame];
-    self.skView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    [self.view addSubview:self.skView];
-    
-    self.coffeeScene = (CoffeeScene *)[SceneManager sceneWithName:CoffeeSceneName];
-    [self.skView presentScene:self.coffeeScene];
+    [self configureForSceneNamed:CoffeeSceneName];
 }
 
 @end
