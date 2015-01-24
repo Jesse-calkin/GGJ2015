2e7323fec8e4a37b1a73ac7cbcc6c76ef3d850f2
Eric Young <eyoung@doubleencore.com>
2015-01-24 11:40:05 -0700

    Add in screen frameworks

--- android/GGJ2015/core/src/com/mygdx/game/MyGdxGame.java
+++ android/GGJ2015/core/src/com/mygdx/game/MyGdxGame.java
@@ -4,24 +4,28 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
-    Texture img;
+    BitmapFont font;
 
     @Override
     public void create() {
+        this.setScreen(new MainGameScreen(this));
+        font = new BitmapFont();
         batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(1, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 0, 0);
-        batch.end();
+        super.render();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
     }
 }
