6bab22a7dc114c4053eee6e4642d798df2012e18
cveazey <Carl_Veazey@cable.comcast.com>
2015-01-24 11:02:31 -0700

    Add a bunch of stub classes, and the APPDELEGATE macro

--- ios/GlobalGameJam/GlobalGameJam/AppDelegate.h
+++ ios/GlobalGameJam/GlobalGameJam/AppDelegate.h
@@ -8,10 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+#define APPDELEGATE (AppDelegate *)[[UIApplication sharedApplication] delegate]
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
 
 @end
 
---
+++ ios/GlobalGameJam/GlobalGameJam/GGJTimeline.m
@@ -0,0 +1,13 @@
+//
+//  GGJTimeline.m
+//  GlobalGameJam
+//
+//  Created by Carl Veazey on 1/24/15.
+//  Copyright (c) 2015 shoshinboogie. All rights reserved.
+//
+
+#import "GGJTimeline.h"
+
+@implementation GGJTimeline
+
+@end
