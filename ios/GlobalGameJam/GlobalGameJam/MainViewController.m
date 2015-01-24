4c33df5780a795115ff3b007527dd6219f3f9912
Chris Weathers <chris@doubleencore.com>
2015-01-24 13:25:16 -0700

    Added test buttons to MainViewController.

--- ios/GlobalGameJam/GlobalGameJam/MainViewController.m
+++ ios/GlobalGameJam/GlobalGameJam/MainViewController.m
@@ -7,11 +7,36 @@
 //
 
 #import "MainViewController.h"
+#import "CoffeeViewController.h"
+#import "HackViewController.h"
+#import "PlanningViewController.h"
+#import "UIViewController+Additions.h"
 
 @interface MainViewController ()
 
 @end
 
 @implementation MainViewController
+
+#pragma mark - Actions
+
+- (IBAction)coffeeButtonTapped {
+    [self switchToViewControllerOfClass:[CoffeeViewController class]];
+}
+
+- (IBAction)hackButtonTapped {
+    [self switchToViewControllerOfClass:[HackViewController class]];
+}
+
+- (IBAction)planningButtonTapped {
+    [self switchToViewControllerOfClass:[PlanningViewController class]];
+}
+
+#pragma mark - Private
+
+- (void)switchToViewControllerOfClass:(Class)viewControllerClass {
+    UIViewController *viewController = [[viewControllerClass alloc] init];
+    [self switchToViewController:viewController completion:nil];
+}
 
 @end
