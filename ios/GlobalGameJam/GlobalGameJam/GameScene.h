d562071d881e20433fad7445f07a381d569a5edc
jesse-calkin <jesse.calkin@gmail.com>
2015-01-23 20:59:55 -0700

    iOS project added

---
+++ ios/GlobalGameJam/GlobalGameJam/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  GlobalGameJam
+//
+
+//  Copyright (c) 2015 shoshinboogie. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ ios/GlobalGameJam/GlobalGameJam/GameScene.m
@@ -0,0 +1,49 @@
+//
+//  GameScene.m
+//  GlobalGameJam
+//
+//  Created by jesse calkin on 1/23/15.
+//  Copyright (c) 2015 shoshinboogie. All rights reserved.
+//
+
+#import "GameScene.h"
+
+@implementation GameScene
+
+-(void)didMoveToView:(SKView *)view {
+    /* Setup your scene here */
+    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    
+    myLabel.text = @"Oregon Trail, but for game jam!";
+    myLabel.fontSize = 15;
+    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
+                                   CGRectGetMidY(self.frame));
+    
+    [self addChild:myLabel];
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    /* Called when a touch begins */
+    
+    for (UITouch *touch in touches) {
+        CGPoint location = [touch locationInNode:self];
+        
+        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
+        
+        sprite.xScale = 0.5;
+        sprite.yScale = 0.5;
+        sprite.position = location;
+        
+        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
+        
+        [sprite runAction:[SKAction repeatActionForever:action]];
+        
+        [self addChild:sprite];
+    }
+}
+
+-(void)update:(CFTimeInterval)currentTime {
+    /* Called before each frame is rendered */
+}
+
+@end
