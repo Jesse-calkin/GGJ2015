24472d8d6964d8e570ae0f117a9920cac6f2d2c6
jesse-calkin <jesse.calkin@gmail.com>
2015-01-23 23:46:20 -0700

    added coffee scene

---
+++ ios/GlobalGameJam/GlobalGameJam/CoffeScene.m
@@ -0,0 +1,13 @@
+//
+//  CoffeScene.m
+//  GlobalGameJam
+//
+//  Created by jesse calkin on 1/23/15.
+//  Copyright (c) 2015 shoshinboogie. All rights reserved.
+//
+
+#import "CoffeScene.h"
+
+@implementation CoffeScene
+
+@end
