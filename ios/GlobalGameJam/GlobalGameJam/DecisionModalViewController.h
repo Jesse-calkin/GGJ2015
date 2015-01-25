74dcb53c2ea4a4959206c972430c1b26037155ce
NickDobos <nickdobos@gmail.com>
2015-01-24 21:54:38 -0700

    1st Pass at Decision Point Popover

---
+++ ios/GlobalGameJam/GlobalGameJam/DecisionModalViewController.h
@@ -0,0 +1,19 @@
+//
+//  DecisionModalViewController.h
+//  GlobalGameJam
+//
+//  Created by Nick Dobos on 1/24/15.
+//  Copyright (c) 2015 shoshinboogie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class GGJDecisionPoint;
+
+@interface DecisionModalViewController : UIViewController
+
+
+- (void)configureWithDecisionPoint:(GGJDecisionPoint *)decisionPoint;
+
+
+@end
