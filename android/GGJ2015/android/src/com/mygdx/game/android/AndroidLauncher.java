95f5d60ce5e92db56c15e5c0b4393c555ecb431a
Brandon Davis <brandon@doubleencore.com>
2015-01-24 22:59:15 -0700

    update coffee game logic

--- android/GGJ2015/android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/GGJ2015/android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -14,6 +14,7 @@ public class AndroidLauncher extends AndroidApplication {
         config.useCompass = false;
         config.useAccelerometer = false;
         config.useImmersiveMode = true;
+        config.useWakelock = true;
         initialize(new MyGdxGame(), config);
     }
 }
